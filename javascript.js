d471368dc9626f9014988dc90d8537ca860e0383
tjuster <tjuster@protonmail.com>
2022-05-10 12:30:04 +0200

    Create computerPlay

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+// Generate a random number between 1 and 3
+function computerPlay () {
+   computerOutput = Math.floor(Math.random() * 3) + 1;
+   let computerString = ('');
+// Assign 1 to 'Rock'
+    if (computerOutput === 1) {
+        computerString = ('Rock');
+        return;
+// Assign 2 to 'Paper'
+    } else if (computerOutput === 2) {
+        computerString = ('Paper');
+        return;
+// Assign 3 to 'Scissors'
+    } else {
+        computerString = ('Scissors');
+        return;
+    }
+}
+
