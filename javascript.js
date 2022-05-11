1709ae54d1c8a03bfae1b0f95d09b3de63966255
tjuster <tjuster@protonmail.com>
2022-05-11 11:00:36 +0200

    Fixed bugs

--- javascript.js
+++ javascript.js
@@ -1,61 +1,53 @@
 // Generate a random number between 1 and 3
-function computerPlay () {
+function computerPlay(computerString) {
    computerOutput = Math.floor(Math.random() * 3) + 1;
-   let computerString = ('');
 // Assign 1 to 'Rock'
     if (computerOutput === 1) {
-        computerString = ('rock');
-    return;
+    computerString = 'rock';
+    return 'rock';
 // Assign 2 to 'Paper'
     } else if (computerOutput === 2) {
-        computerString = ('paper');
-    return;
+    computerString = 'paper';
+    return 'paper';
 // Assign 3 to 'Scissors'
     } else {
-        computerString = ('scissors');
-    return;
+    computerString = 'scissors';
+    return 'scissors';
     }
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound (playerSelection, computerSelection) {
 // Let user input 'Rock', 'Paper' or 'Scissors', case-insenstive
-    playerSelection = window.prompt('Do you want to play Rock, Paper or Scissors?').toLowerCase();
+        playerSelection = window.prompt('Do you want to play Rock, Paper or Scissors?').toLowerCase();
+        console.log(playerSelection);
 // Check if it's not a draw
-    if (playerSelection === 'rock' && computerSelection === 'rock') {
-    console.log('It\'s a draw!')
-    return;
-    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
-    console.log('It\'s a draw!')
-    return;
-    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
-    console.log('It\'s a draw!')
-    return;
+        if (computerSelection === 'rock' && playerSelection === 'rock') {
+        return 'It\'s a draw!';
+        } else if (computerSelection === 'paper' && playerSelection === 'paper') {
+        return 'It\'s a draw!';
+        } else if (computerSelection === 'scissors' && playerSelection === 'scissors') {
+        return 'It\'s a draw!';
 // Compare input to computerPlay
-    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-    console.log('You lose! Paper beats Rock!')
-    return;
-    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-    console.log('You win! Rock beats Scissors!')
-    return;
-    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-    console.log('You win! Paper beats Rock!')
-    return;
-    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-    console.log('You lose! Scissors beats Paper!')
-    return;
-    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-    console.log('You lose! Rock beats Scissors!')
-    return;
-    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-    console.log('You win! Scissors beats Paper!')
-    return;
+        } else if (computerSelection === 'paper' && playerSelection === 'rock') {
+        return 'You lose! Paper beats Rock!';
+        } else if (computerSelection === 'scissors' && playerSelection === 'rock') {
+        return 'You win! Rock beats Scissors!';
+        } else if (computerSelection === 'rock' && playerSelection === 'paper') {
+        return 'You win! Paper beats Rock!';
+        } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
+        return 'You lose! Scissors beats Paper!';
+        } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
+        return 'You lose! Rock beats Scissors!';
+        } else if (computerSelection === 'paper' && playerSelection === 'scissors') {
+        return 'You win! Scissors beats Paper!';
 // Check if they didn't input anything else
-    } else {
-    console.log('You didn\'t input Rock, Paper or Scissors!')
-    return;
-    }
-  }
-  
-  const playerSelection = 'rock';
-  const computerSelection = computerPlay();
-  console.log(playRound(playerSelection, computerSelection));
+        } else {
+        return 'You didn\'t input Rock, Paper or Scissors';
+        }
+}
+
+const computerString = "rock";
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+console.log(computerSelection)
