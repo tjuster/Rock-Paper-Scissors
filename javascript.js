a5f571440af5a82e2cba16b8978b6bfad877b6f5
tjuster <tjuster@protonmail.com>
2022-05-17 12:57:12 +0200

    Add buttons and eventlisteners

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-alert('Welcome to Tjuster\'s Rock, Paper and Scissors game! You will play five rounds, you get one point for winning and no points for a draw. At the end a winner will be declared. You will have to enter rock, paper or scissors manually each time (case-insensitive). You will get a popup with the computer\'s selection and wheter you\'ve won or lost the round, or if it was a draw. At the end a popup will appear with how many rounds you won and lost and who\'s the winner or loser of the whole game. The game can also end in a draw. Thanks for playing!')
-
 // Generate a random number between 1 and 3
 function computerPlay() {
 computerOutput = Math.floor(Math.random() * 3) + 1;
@@ -16,12 +14,9 @@ computerOutput = Math.floor(Math.random() * 3) + 1;
 }
      
 function playRound (playerSelection, computerSelection) {
+        console.log(playerSelection);
 // Check if it's not a draw
-        if (computerSelection === 'rock' && playerSelection === 'rock') {
-        return 'It\'s a draw!';
-        } else if (computerSelection === 'paper' && playerSelection === 'paper') {
-        return 'It\'s a draw!';
-        } else if (computerSelection === 'scissors' && playerSelection === 'scissors') {
+        if (computerSelection === playerSelection) {
         return 'It\'s a draw!';
 // Compare input to computerPlay
         } else if (computerSelection === 'paper' && playerSelection === 'rock') {
@@ -46,36 +41,24 @@ function playRound (playerSelection, computerSelection) {
         } else {
         return 'You didn\'t input rock, paper or scissors.';
         }
+
 }
 
-// Define global variables
-let     gameWins = 0;
-let     gameLosses = 0;
-let     playerSelection = 'rock';
+let computerSelection = computerPlay();
+let playerSelection = 'rock';
 
-// Loop 5 games
-for (let i = 0; i < 5; i++) {
-        computerPlay();
-        let computerSelection = computerPlay();
-        playerSelection = window.prompt('Do you want to play rock, paper or scissors?').toLowerCase();
-        alert(`You selected ${playerSelection}!`);
-        alert(`The computer selected ${computerSelection}!`);
-        playRound (playerSelection, computerSelection);
-        alert(playRound (playerSelection, computerSelection));
-}
+const btnRock = document.querySelector('.rock');
+btnRock.addEventListener('click', () => playRound(), {
+  playerSelection : 'rock'
+});
 
-// Divides the gameWins and gameLosses by two
-gameWins = gameWins / 2;
-gameLosses = gameLosses / 2;
+const btnPaper = document.querySelector('.paper');
+btnPaper.addEventListener('click', () => playRound(), {
+  playerSelection : 'paper'
+});
 
-// Announce how many games are won and how many games are lost
-alert(`You won ${gameWins} rounds and lost ${gameLosses} rounds!`)
+const btnScissors = document.querySelector('.scissors');
+btnScissors.addEventListener('click', () => playRound(), {
+  playerSelection : 'scissors'
+});
 
-// Announce a winner, loser or if it's a draw
-if (gameWins > gameLosses) {
-        alert('Congratulations, you\'ve won the game!')
-} else if (gameLosses > gameWins) {
-        alert('Too bad, you\'ve lost the game..')
-} else {
-        alert('It\'s a draw, nobody won or lost the game!')
-}
